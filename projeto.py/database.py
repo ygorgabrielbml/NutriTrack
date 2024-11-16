46d117a96758a26281533f48df3031a6acb601f3
henrique-bees <riquedemoraes424@gmail.com>
2024-11-15 21:53:12 -0300

    adição do banco de dados e alterações no codigo para fazer a conexao

--- projeto.py/database.py
+++ projeto.py/database.py
@@ -1,23 +1,9 @@
 import sqlite3
 
-#criação do código e do cursos
-con = sqlite3.connect("registro.db")
-cursor = con.cursor()
-
-#criação do banco de dados os valores de registro
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS dados(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    login TEXT UNIQUE NOT NULL,
-    senha TEXT NOT NULL)
-""")
-con.commit()
-cursor.close()
-con.close()
 
 #função para adcionar usuário
 def adcionar_usuario(login, senha):
-    con = sqlite3.connect("registro.db")
+    con = sqlite3.connect("./NutriTrack/source/models/database.db")
     cursor = con.cursor()
     
     try:
@@ -32,7 +18,7 @@ def adcionar_usuario(login, senha):
 
 #função para remover usuário
 def deletar_usuario(login):
-    con = sqlite3.connect("registro.db")
+    con = sqlite3.connect("./NutriTrack/source/models/database.db")
     cursor = con.cursor()
 
     cursor.execute("DELETE FROM dados where login = ?", (login,))
